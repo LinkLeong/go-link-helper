e507192f653a6f598926ff7b032ae0c7c7cadb8b
a624669980 <a624669980@163.com>
2021-04-12 21:39:33 +0800

    修改redis

--- redis/redis.go
+++ redis/redis.go
@@ -7,11 +7,14 @@ import (
 var RedisConn *gredis.Client
 
 //初始化redis
-func Setup() {
+// url 连接地址  如：192.168.200.159：6379
+// pwd 密码  如：123456
+// db 几号数据库 如：3
+func Setup(url,pwd string,db int) {
 	RedisConn = gredis.NewClient(
 		&gredis.Options{
-			Addr: "",
-			Password: "",
-			DB:3,
+			Addr: url,
+			Password: pwd,
+			DB:db,
 		})
 }
