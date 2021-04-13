e1a32fab83c56a03ec23f973873f863d063e81fd
a624669980 <a624669980@163.com>
2021-04-13 12:02:50 +0800

    添加consul注册

---
+++ a_consul/consul.go
@@ -0,0 +1,55 @@
+package a_consul
+
+import (
+	"fmt"
+	consulapi "github.com/hashicorp/consul/api"
+	"github.com/hashicorp/go-uuid"
+)
+
+
+type ConsulOpt struct {
+	//consul agent的地址 例如：192.168.200.147:8500
+	ConsulAddress  string
+	//当前客户端ip，注册到consul的IP地址
+	LocalIp string
+	//当前程序启用的端口
+	LocalPort int
+	//服务名称
+	Name string
+	//标签
+	Tags[] string
+}
+
+//服务注册与发现
+func Setup( op ConsulOpt) error {
+	// 创建连接consul服务配置
+	config := consulapi.DefaultConfig()
+	config.Address = op.ConsulAddress
+	client, err := consulapi.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	id,_:=uuid.GenerateUUID()
+	// 创建注册到consul的服务到
+	registration := new(consulapi.AgentServiceRegistration)
+	registration.ID = id
+	registration.Name = op.Name
+	registration.Port = op.LocalPort
+	registration.Tags = []string{"testService"}
+	registration.Address = op.LocalIp
+
+	// 增加consul健康检查回调函数
+	check := new(consulapi.AgentServiceCheck)
+	check.HTTP = fmt.Sprintf("http://%s:%d/get/check", registration.Address, registration.Port)
+	check.Timeout = "5s"
+	check.Interval = "5s"
+	check.DeregisterCriticalServiceAfter = "30s" // 故障检查失败30s后 consul自动将注册服务删除
+	registration.Check = check
+
+	// 注册服务到consul
+	err = client.Agent().ServiceRegister(registration)
+	return err
+}
+
+
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v8 v8.8.0
+	github.com/hashicorp/consul/api v1.8.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
